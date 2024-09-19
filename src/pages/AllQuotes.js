41beba4676bace6c51b1037d6d8d4f2cae701264
eid <eidhachem1@gmail.com>
2024-09-19 23:29:31 +0300

    Add quotes list and form

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,17 @@
 import React from "react"
+import QuoteList from "../components/quotes/QuoteList"
+
+const DUMMY_DATA = [
+  { id: "q1", auther: "Eid", text: "React is better than vue" },
+  { id: "q2", auther: "Hachem", text: "React is better than Angular too" },
+]
 
 const AllQuotes = () => {
-  return <div>AllQuotes</div>
+  return (
+    <div>
+      <QuoteList quotes={DUMMY_DATA} />
+    </div>
+  )
 }
 
 export default AllQuotes
