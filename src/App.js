98bbffeac006dec1b83e05c14c91cd19d446a334
eid <eidhachem1@gmail.com>
2024-09-20 17:22:42 +0300

    Optimize code by using lazy loading

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
+import React, { Suspense } from "react"
 import { Route, Routes, Navigate } from "react-router-dom"
-import AllQuotes from "./pages/AllQuotes"
-import QuoteDetail from "./pages/QuoteDetail"
-import NewQuote from "./pages/NewQuote"
 import Layout from "./components/layout/Layout"
-import NotFound from "./pages/NotFound"
+import LoadingSpinner from "./components/UI/LoadingSpinner"
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"))
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"))
+const NotFound = React.lazy(() => import("./pages/NotFound"))
+const AllQuotes = React.lazy(() => import("./pages/AllQuotes"))
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/quotes" />}></Route>
-        <Route path="/quotes" element={<AllQuotes />}></Route>
-        <Route path="/quotes/:quoteId/*" element={<QuoteDetail />}></Route>
-        <Route path="/new-quote" element={<NewQuote />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/quotes" />}></Route>
+          <Route path="/quotes" element={<AllQuotes />}></Route>
+          <Route path="/quotes/:quoteId/*" element={<QuoteDetail />}></Route>
+          <Route path="/new-quote" element={<NewQuote />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Suspense>
     </Layout>
   )
 }
