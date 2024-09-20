bd17a5f411a33105517e1cbe472048a0ef52bd2f
eid <eidhachem1@gmail.com>
2024-09-20 08:40:26 +0300

    Add loginc to sort quotes

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -7,7 +7,6 @@ const NewQuote = () => {
   const history = useHistory()
 
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData)
     history.push("/quotes")
   }
   return <QuoteForm onAddQuote={addQuoteHandler} />
